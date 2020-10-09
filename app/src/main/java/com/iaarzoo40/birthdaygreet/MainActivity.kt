9ccfe80c1140818b3b68ec67c2288b0da3df0fff
Aarzoo <iaarzoo40@gmail.com>
2020-10-09 09:49:10 +0530

    Hello..this is my birthday greet android project.

---
+++ app/src/main/java/com/iaarzoo40/birthdaygreet/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.iaarzoo40.birthdaygreet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name2 = nameInput.editableText.toString()
+        Toast.makeText(this, "name is $name2", Toast.LENGTH_SHORT).show()
+        val name = nameInput.editableText.toString()
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+    }
+}
